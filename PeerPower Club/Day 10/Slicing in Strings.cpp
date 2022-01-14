fad26cf3a843210b840db49cd2aac1c8f6ef9c81
joel0901 <97008364+joel0901@users.noreply.github.com>
2022-01-14 10:42:24 +0530

    Add files via upload

---
+++ PeerPower Club/Day 10/Slicing in Strings.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main()
+{
+	
+	string hello = "Hello! I'm Joel. This is an example";
+	cout<<hello.substr(6, 9);
+	
+	return 0;
+}
