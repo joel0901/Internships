3b66f5cb5b051f77ee6d6b859ab5574cf888cce5
joel0901 <97008364+joel0901@users.noreply.github.com>
2022-01-14 10:35:31 +0530

    Add files via upload

---
+++ PeerPower Club/Day 8/Function Parameters.cpp
@@ -0,0 +1,15 @@
+#include <iodtream>
+using namespace std;
+
+
+void myFunction(string fname) {
+  cout << fname << " Refsnes\n";
+}
+
+int main() {
+  myFunction("Liam");
+  myFunction("Jenny");
+  myFunction("Anja");
+  return 0;
+}
+
