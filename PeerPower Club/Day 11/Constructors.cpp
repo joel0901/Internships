b38327d7f04cafc7a55936d1c53628f08b2dd4df
joel0901 <97008364+joel0901@users.noreply.github.com>
2022-01-14 11:04:36 +0530

    Add files via upload

---
+++ PeerPower Club/Day 11/Constructors.cpp
@@ -0,0 +1,35 @@
+// C++ program to demonstrate constructors
+
+#include <iostream>
+using namespace std;
+class Geeks
+{
+	public:
+	int id;
+	
+	//Default Constructor
+	Geeks()
+	{
+		cout << "Default Constructor called" << endl;
+		id=-1;
+	}
+	
+	//Parameterized Constructor
+	Geeks(int x)
+	{
+		cout << "Parameterized Constructor called" << endl;
+		id=x;
+	}
+};
+int main() {
+	
+	// obj1 will call Default Constructor
+	Geeks obj1;
+	cout << "Geek id is: " <<obj1.id << endl;
+	
+	// obj1 will call Parameterized Constructor
+	Geeks obj2(21);
+	cout << "Geek id is: " <<obj2.id << endl;
+	return 0;
+}
+
